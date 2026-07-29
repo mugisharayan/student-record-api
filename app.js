44762d549e5385dee15989e73b1ca3bc8b141f3d
mugisharayan <mgishrayan@gmail.com>
2026-07-29 09:59:51 +0300

    starting the server

--- app.js
+++ app.js
@@ -3,3 +3,24 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+
+// in-memory data (Temporary database)
+
+// GET all students
+
+// GET one student
+
+// POST - Add a new student
+
+// Validation
+
+// PATCH - Update a student
+
+// DELETE - Remove a student
+
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
