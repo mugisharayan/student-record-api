e09751344f41b43b7bd68002208af9d838724f19
CodinBbg <ishoborabyoseclementine@gmail.com>
2026-07-29 11:34:43 +0300

    Get students

--- app.js
+++ app.js
@@ -7,8 +7,23 @@ app.use(express.json());
 // in-memory data (Temporary database)
 
 // GET all students
+app.get("/students", (req, res) => {
+    res.status(200).json(students);
+});
 
 // GET one student
+app.get("/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    
+    const student = students.find((s) => s.id === id);
+
+    if (!student) {
+        return res.status(404).json({ 
+            error: "Student not found"
+        });
+    }
+    res.status(200).json(student);
+});
 
 // POST - Add a new student
 
