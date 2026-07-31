61707a1c39da6ff67179a19c4d2a09d9e56a8978
Festus Asembo <asembof@gmail.com>
2026-07-31 01:33:06 +0100

    Use process.env.PORT for Render deployment

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.delete("/students/:id", (req, res) => {
     });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
